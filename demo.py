865d94ef89aac93bec64bca11950ddf52b0fb139
Alex Morega <alex@grep.ro>
2012-07-15 14:44:23 +0300

    open/close database session; create tables

--- demo.py
+++ demo.py
@@ -1,11 +1,18 @@
+import sqlite3
 import flask
 from flask.ext.htables import HTables
 
 
+connection = sqlite3.connect(':memory:')
+
+
 app = flask.Flask(__name__)
-htables = HTables(app)
+htables = HTables(app, tables=['person', 'sport'],
+                  get_connection=lambda: connection)
 app.register_blueprint(htables.admin, url_prefix='/admin')
 
 
 if __name__ == '__main__':
+    with app.test_request_context():
+        htables.session.create_all()
     app.run(debug=True)
--- flask_htables.py
+++ flask_htables.py
@@ -1,4 +1,5 @@
 import flask
+import htables
 
 
 admin_blueprint = flask.Blueprint('htables_admin', __name__)
@@ -10,10 +11,39 @@ def index():
 
 class HTables(object):
 
-    def __init__(self, app=None):
-        if app is not None:
-            self.initialize_app(app)
+    def __init__(self, app, tables, get_connection):
+        self.schema = htables.Schema()
+        for name in tables:
+            self.schema.define_table(name, name)
+        self.get_connection = get_connection
+        self.initialize_app(app)
         self.admin = admin_blueprint
 
     def initialize_app(self, app):
-        pass
+        app.teardown_request(self._close_database)
+
+    def _close_database(self, err):
+        top = flask._request_ctx_stack.top
+        if not hasattr(top, 'htables_session'):
+            return
+        try:
+            if err is None:
+                top.htables_session.commit()
+            else:
+                top.htables_session.rollback()
+            del top.htables_session
+        except:
+            flask.current_app.logger.exception("Failed to close database")
+
+    def _get_or_create_session(self):
+        top = flask._request_ctx_stack.top
+        if top is None:
+            raise RuntimeError('working outside of request context')
+        if not hasattr(top, 'htables_session'):
+            top.htables_session = htables.SqliteSession(
+                    self.schema, self.get_connection(), {})
+        return top.htables_session
+
+    @property
+    def session(self):
+        return self._get_or_create_session()
