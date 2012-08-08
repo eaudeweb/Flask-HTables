35bf30d60563f3e8a7ae473db1148840357cf520
Alex Morega <alex@grep.ro>
2012-08-08 14:36:54 +0300

    tests for autocommit and autorollback

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 import unittest
+import tempfile
+import shutil
+import sqlite3
 import flask
 
 
@@ -8,7 +11,6 @@ class DatabaseAccessTest(unittest.TestCase):
         import flask_htables
         app = flask.Flask(__name__)
         def connect():
-            import sqlite3
             return sqlite3.connect(':memory:')
         ht = flask_htables.HTables(app, [], connect)
         with app.test_request_context():
@@ -17,3 +19,36 @@ class DatabaseAccessTest(unittest.TestCase):
             person = table.new(hello="world")
             person.save()
             self.assertEqual(table.find_first(), {"hello": "world"})
+
+
+class DatabaseAutocommitTest(unittest.TestCase):
+
+    def setUp(self):
+        import flask_htables
+        self.tmp = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, self.tmp)
+        self.app = flask.Flask(__name__)
+        def connect():
+            return sqlite3.connect(self.tmp + '/db.sqlite')
+        self.ht = flask_htables.HTables(self.app, [], connect)
+        with self.app.test_request_context():
+            self.ht.session['person'].create_table()
+
+    def test_commit_on_success(self):
+        with self.app.test_request_context():
+            self.ht.session['person'].new({'name': 'black'}).save()
+            # autocommit should occur here
+
+        with self.app.test_request_context():
+            self.ht.session.rollback()
+            self.assertEqual(list(self.ht.session['person'].find()),
+                             [{'name': 'black'}])
+
+    def test_rollback_on_error(self):
+        with self.assertRaises(ValueError):
+            with self.app.test_request_context():
+                self.ht.session['person'].new({'name': 'black'}).save()
+                raise ValueError # should trigger rollback
+
+        with self.app.test_request_context():
+            self.assertEqual(list(self.ht.session['person'].find()), [])
